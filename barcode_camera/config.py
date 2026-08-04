c79d4c2eebc8d9ddc1f194db5f1ac63664893ec9
J.N.Zwamborn <Joeri.Zwamborn@hotmail.com>
2026-08-04 15:07:04 +0200

    Add feed.py barebones and update config.py

--- barcode_camera/config.py
+++ barcode_camera/config.py
@@ -1,1 +1,25 @@
-Import config.yaml
+class Station:
+    def __init__(self, raw):
+        self.name = raw ['name']
+
+class Camera:
+    def __init__(self, raw):
+        self.index = raw ['index']
+
+class Scanner:
+    def __init__(self, raw):
+        self.device = raw ['device']
+
+class Storage:
+    def __init__(self, raw):
+        self.loc_dir = raw ['local_directory']
+
+class sharepoint:
+    def __init__(self, raw):
+        self.url = raw ['drive_id']
+        self.username = raw ['client_id']
+        self.password = raw ['client_secret']
+        self.folder = raw ['folder']
+        self.enabled = raw ['enabled']
+
+config = Config(yaml.safe_load(open('config.example.yaml')))
