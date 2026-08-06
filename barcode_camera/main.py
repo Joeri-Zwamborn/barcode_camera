ba029fa9629332c2593f6843986bcf5ed9f5c50b
Joeri.Zwamborn <Joeri.Zwamborn@hotmail.com>
2026-08-06 15:36:42 +0200

    add .log file

--- barcode_camera/main.py
+++ barcode_camera/main.py
@@ -1,14 +1,21 @@
 from camera import Camera
 from scanner import BarcodeScanner
 from storage import save_image
-import logging
-
 from config import CAMERA_INDEX
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+LOG_FILE_PATH = Path(__file__).parent / "camera.log"
 
 logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    )
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_PATH, maxBytes=1024*1024, backupCount=5),
+        logging.StreamHandler()
+    ]
+)
 logger = logging.getLogger(__name__)
 
 print("Ready to scan barcodes. Please scan a barcode to capture an image.")
@@ -25,6 +32,7 @@ def main():
             save_image(barcode, frame)
     except Exception:
         logger.exception("An error occurred while scanning barcodes.")
+
     finally:
         camera.close()
         logging.info("Camera closed. Exiting program.")
